6603df18f0a26fb1e2569f997844a5859825efea
eliott-colin <eliott.colin.pro@gmail.com>
2025-10-15 12:22:19 +0200

    base /

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/ci-cd/",
+  base: "/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)), // 👈 alias pour `@`
